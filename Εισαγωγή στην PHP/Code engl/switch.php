22a28fb04f3c7e37070f47c191dd36e0c58db499
Konstantinos <ksakkasuoi@gmail.com>
2020-07-24 14:31:44 +0300

    switch in php

---
+++ Εισαγωγή στην PHP/Code engl/switch.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$color = "red";
+
+switch ($color) {
+  case "red":
+    echo "color is red!";
+    break;
+  case "blue":
+    echo "color is blue!";
+    break;
+  case "green":
+    echo "color is green!";
+    break;
+  default:
+    echo "color is neither red, blue, nor green!";
+}
+?>
+
+</body>
+</html>
---
+++ Εισαγωγή στην PHP/Code greek/switch.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$color = "κόκκινο";
+
+switch ($color) {
+  case "κόκκινο":
+    echo "Το χρώμα ειναι κόκκινο";
+    break;
+  case "μπλε":
+    echo "Το χρώμα είναι μπλε";
+    break;
+  case "πράσινο":
+    echo "Το χρώμα είναι πράσινο";
+    break;
+  default:
+    echo "Το χρώμα δεν είναι κανένα απο τα παραπάνω";
+}
+?>
+
+</body>
+</html>
