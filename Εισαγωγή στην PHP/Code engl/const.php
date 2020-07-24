f0a36673c5a431246cb2ad67ab57a6fb7b4d272e
Konstantinos <ksakkasuoi@gmail.com>
2020-07-24 13:49:01 +0300

    const in php

---
+++ Εισαγωγή στην PHP/Code engl/const.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+    define("dit", "Department Informatics and Telecomunication"); //value cannot be changed
+    echo dit;
+?>
+
+</body>
+</html>
---
+++ Εισαγωγή στην PHP/Code greek/const.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+    define("dit", "Department Informatics and Telecomunication"); //η τιμή δεν μπορεί να αλλάξει
+    echo dit;
+?>
+
+</body>
+</html>
