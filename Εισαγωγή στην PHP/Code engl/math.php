5085c6be10ed53af8c995fb0a8a077198a9413a8
Konstantinos <ksakkasuoi@gmail.com>
2020-07-24 13:18:43 +0300

    math function in php

---
+++ Εισαγωγή στην PHP/Code engl/math.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+    echo(pi()); // returns 3.1415926535898
+
+    echo(min(24, 348, 10, 35, 289, 528, 1));  // return min number
+    echo(max(24, 348, 10, 35, 289, 528, 1));  // return max number
+    
+    echo(abs(-6));  // return absolute value
+
+    echo(sqrt(64));  // return square root of a number
+
+    echo(round(1.8));  // return rounds a floating-point number to its nearest integer
+    echo(round(1.3));  // return rounds a floating-point number to its nearest integer
+
+    echo(rand()); // return random number
+?>
+
+</body>
+</html>
