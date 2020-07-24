5005ae0b602f099b853815dc0d0c9f6621f583b8
Konstantinos <ksakkasuoi@gmail.com>
2020-07-24 14:05:26 +0300

    operator in php

---
+++ Εισαγωγή στην PHP/Code engl/operator.php
@@ -0,0 +1,50 @@
+<!--
+PHP Operators:
+
++	Addition
+-	Subtraction
+*	Multiplication
+**	Exponentiation (ES2016)
+/	Division
+%	Modulus (Division Remainder)
+++	Increment
+--	Decrement
+
+=   => a = b
++=  => a = a + b
+-=	=> a = a - b
+*=	=> a = a * b
+/=	=> a = a / b
+%=	=> a = a % b
+
+==	equal to
+===	equal value and equal type
+!=	not equal
+<>  not equal	
+!==	not equal value or not equal type
+>	greater than
+<	less than
+>=	greater than or equal to
+<=	less than or equal to
+<=> spaceship
+
+and	 logical and
+or	 logical or
+xor	 logical xor
+&&	 logical and
+||	 logical or
+!	 logical not
+
+.	concatenation
+.=	concatenation assignment
+
++	union
+==	equality
+===	identity
+!=	inequality
+<>	inequality
+!==	non-identity	
+
+?:	ternary
+??	null coalescing	
+-->
