07effb87315d4c9c6048bbba41d27dd16dd64e0e
Konstantinos <ksakkasuoi@gmail.com>
2020-07-24 09:55:33 +0300

    print in php

---
+++ Εισαγωγή στην PHP/Code engl/print.php
@@ -0,0 +1,14 @@
+<!--The differences are small: echo has no return value while print has a return value of 1 
+so it can be used in expressions. echo can take multiple parameters (although such usage is rare) 
+while print can take one argument. echo is marginally faster than print.-->
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$s = "Kostas"; //variable statement
+print "My name is $s"; //display variable content
+?>
+
+</body>
+</html>
