584b896bbaf1eb9eb49fbbd5fdbd2c9eb41f46aa
Konstantinos <ksakkasuoi@gmail.com>
2020-07-24 18:59:57 +0300

    function in php

---
+++ Εισαγωγή στην PHP/Code engl/function.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+    function mes() { //function
+        echo "Hello world!";
+    }
+    
+    mes(); // call the function
+?>
+
+</body>
+</html>
---
+++ Εισαγωγή στην PHP/Code greek/function.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+    function mes() { // Συνάρτηση
+        echo "Hello world!";
+    }
+    
+    mes(); // Κλήση συνάρτησης
+?>
+
+</body>
+</html>
