cb0b22364a6f08e5770fca8f74a3f728042d5675
Konstantinos <ksakkasuoi@gmail.com>
2020-07-24 14:28:07 +0300

    if in php

---
+++ Εισαγωγή στην PHP/Code engl/if.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+    $s = 13;
+    if($s<12)
+    {
+        echo "good morning";
+    }
+    else
+    {
+        echo "good day";
+    }
+?>
+
+</body>
+</html>
---
+++ Εισαγωγή στην PHP/Code greek/if.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+    $s = 13;
+    if($s<12)
+    {
+        echo "Καλή μέρα";
+    }
+    else
+    {
+        echo "Καλό μεσημέρι";
+    }
+?>
+
+</body>
+</html>
