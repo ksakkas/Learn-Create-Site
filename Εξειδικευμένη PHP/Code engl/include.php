f2cac7c1e3cbffe3a2a2ec642cad1212ea8771f0
Konstantinos <ksakkasuoi@gmail.com>
2020-07-26 12:18:40 +0300

    file for include in php

---
+++ Εξειδικευμένη PHP/Code engl/include.php
@@ -0,0 +1,10 @@
+<!--The include (or require) statement takes all the text/code/markup that exists in the specified 
+file and copies it into the file that uses the include statement.
+
+Including files is very useful when you want to include the same PHP, HTML, or text on multiple
+pages of a website.
+-->
+
+<?php
+echo "Department Informatics and Telecomunication";
+?>
