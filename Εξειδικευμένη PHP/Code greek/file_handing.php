cff58450bd791f78ce080d0efcf4126ab12c1dea
Konstantinos <ksakkasuoi@gmail.com>
2020-07-26 12:24:48 +0300

    read file from txt in php

---
+++ Εξειδικευμένη PHP/Code greek/file_handing.php
@@ -0,0 +1,22 @@
+<!--
+    AJAX = Asynchronous JavaScript and XML
+    CSS = Cascading Style Sheets
+    HTML = Hyper Text Markup Language
+    PHP = PHP Hypertext Preprocessor
+    SQL = Structured Query Language
+    SVG = Scalable Vector Graphics
+    XML = EXtensible Markup Language
+-->
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo readfile("word.txt");
+?>
+
+</body>
+</html>
+
