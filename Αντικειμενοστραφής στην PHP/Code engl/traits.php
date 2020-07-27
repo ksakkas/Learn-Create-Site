2f36093aa021f4e8297ff276f2e3399c0ece1fba
Konstantinos <ksakkasuoi@gmail.com>
2020-07-27 12:43:44 +0300

    traits in php

---
+++ Αντικειμενοστραφής στην PHP/Code engl/traits.php
@@ -0,0 +1,41 @@
+<!--
+PHP only supports single inheritance: a child class can inherit only from one single parent.
+
+So, what if a class needs to inherit multiple behaviors? OOP traits solve this problem.
+
+Traits are used to declare methods that can be used in multiple classes. 
+Traits can have methods and abstract methods that can be used in multiple classes, 
+and the methods can have any access modifier (public, private, or protected).
+
+Traits are declared with the trait keyword:
+-->
+
+<?php
+trait TraitName {
+  // some code...
+}
+?>
+
+<!--To use a trait in a class, use the use keyword:-->
+
+<?php
+class MyClass {
+  use TraitName;
+}
+?>
+
+<?php
+trait message1 {
+public function msg1() {
+    echo "OOP is fun! ";
+  }
+}
+
+class Welcome {
+  use message1;
+}
+
+$obj = new Welcome();
+$obj->msg1();
+?>
+
