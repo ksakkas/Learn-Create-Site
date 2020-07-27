0aa8c0bfd670b9c02f917ad163a94600c16dc816
Konstantinos <ksakkasuoi@gmail.com>
2020-07-27 10:23:34 +0300

    file upload in php

--- Εξειδικευμένη PHP/Code engl/file_upload.php
+++ Εξειδικευμένη PHP/Code engl/file_upload.php
@@ -1,11 +1,13 @@
 <?php
-   if(isset($_FILES['image'])){
+   if(isset($_FILES['image'])){ //The isset() function checks whether a variable is set, which means that it has to be declared and is not NULL.
       $errors= array();
-      $file_name = $_FILES['image']['name'];
-      $file_size =$_FILES['image']['size'];
-      $file_tmp =$_FILES['image']['tmp_name'];
-      $file_type=$_FILES['image']['type'];
-      $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+      $file_name = $_FILES['image']['name']; //name img
+      $file_size =$_FILES['image']['size']; //size img
+      $file_tmp =$_FILES['image']['tmp_name']; //temporary  img
+      $file_type=$_FILES['image']['type']; //type img
+      $file_ext=strtolower(end(explode('.',$_FILES['image']['name']))); //The strtolower() function converts a string to lowercase.
+      //The explode() function breaks a string into an array.
+
       
       $expensions= array("jpeg","jpg","png");
       
@@ -27,7 +29,7 @@
 ?>
 <html>
    <body>
-      
+      <!--The enctype attribute specifies how the form-data should be encoded when submitting it to the server.-->
       <form action="" method="POST" enctype="multipart/form-data">
          <input type="file" name="image" />
          <input type="submit"/>
--- Εξειδικευμένη PHP/Code greek/file_upload.php
+++ Εξειδικευμένη PHP/Code greek/file_upload.php
@@ -1,11 +1,12 @@
 <?php
-   if(isset($_FILES['image'])){
+   if(isset($_FILES['image'])){ //Η συνάρτηση Isset () ελέγχει εάν έχει οριστεί μια μεταβλητή, πράγμα που σημαίνει ότι πρέπει να δηλωθεί και να μην είναι NULL.
       $errors= array();
-      $file_name = $_FILES['image']['name'];
-      $file_size =$_FILES['image']['size'];
-      $file_tmp =$_FILES['image']['tmp_name'];
-      $file_type=$_FILES['image']['type'];
-      $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+      $file_name = $_FILES['image']['name']; //όνομα εικόνας
+      $file_size =$_FILES['image']['size']; //μέγεθος εικόνας
+      $file_tmp =$_FILES['image']['tmp_name']; //πληροφοριες εικόνας
+      $file_type=$_FILES['image']['type']; //τύπος εικόνας
+      $file_ext=strtolower(end(explode('.',$_FILES['image']['name']))); //Η συνάρτηση strtolower () μετατρέπει μια συμβολοσειρά σε πεζά.
+      //Η συνάρτηση explode () σπάει μια συμβολοσειρά σε έναν πίνακα.
       
       $expensions= array("jpeg","jpg","png");
       
@@ -27,7 +28,7 @@
 ?>
 <html>
    <body>
-      
+      <!--Το χαρακτηριστικό enctype καθορίζει τον τρόπο με τον οποίο τα δεδομένα φόρμας πρέπει να κωδικοποιούνται κατά την υποβολή τους στο διακομιστή.-->
       <form action="" method="POST" enctype="multipart/form-data">
          <input type="file" name="image" />
          <input type="submit"/>
